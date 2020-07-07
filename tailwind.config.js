8c626e3b3d5a3f4dad4cfb50bc47ad926ed09fe4
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-07-07 21:41:46 +0200

    update hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
     extend: {
       transitionDuration: {
-        "2000": "2000ms"
+        "2000": "2000ms",
       },
       margin: {
-        "-128": "-32rem"
+        "-128": "-32rem",
       },
       transitionTimingFunction: {
-        "ease-rubber": "steps(5, jump-end)"
+        "ease-rubber": "steps(5, jump-end)",
       },
       spacing: {
         "1280px": "1280px",
-        "720px": "720px"
+        "720px": "720px",
       },
       inset: {
-        "-200px": "-200px"
-      }
-    }
+        "-200px": "-200px",
+      },
+      fontFamily: {
+        sans: ["'Noto Sans'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
